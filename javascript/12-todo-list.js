9208eb3c5e4c7d311289c6e4242f13bed22a3a5d
DM <danymartinezluna@gmail.com>
2023-08-10 19:26:58 -0400

    finished part 1 of lesson 12

---
+++ javascript/12-todo-list.js
@@ -0,0 +1,32 @@
+const todoList =[{name:'make',
+dueDate:'2023-08-09'},{name:'dinner', dueDate: '2023-08-09'}];
+
+renderTodoList();
+function renderTodoList(){
+  let todoListHTML='';
+
+  todoList.forEach(function(todoObject,index){
+    const {name,dueDate}=todoObject;
+      const html = `
+        <div>${name}</div>
+        <div> ${dueDate}</div>
+        <button onclick="todoList.splice(${index},1);renderTodoList();" class="delete-todo-button">Delete</button>
+      `;
+    todoListHTML+=html;
+  })
+  document.querySelector('.js-todo-list').innerHTML =todoListHTML;
+}
+
+
+function addTodo(){
+  const inputElemnt =document.querySelector('.js-name-input');
+  const dateInputElement= document.querySelector('.js-due-date-input');
+  const name = inputElemnt.value;
+  const dueDate =dateInputElement.value;
+
+  todoList.push({name, dueDate});
+  console.log(todoList);
+  inputElemnt.value = '';
+  dateInputElement.value='';
+  renderTodoList();
+}
